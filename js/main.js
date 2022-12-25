4195c205b4740a58f2232e2e430b58b60f5e4cce
Bacon <bacon.antife@gmail.com>
2022-12-25 20:43:44 -0300

    Pause, full screen and reset buttons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+let mainWidth;
+let mainHeight;
+let x0, y0;
+let playerX0;
+let playerXOffset;
+let cx, cy;
+let clockRadius;
+let clockArcThickness;
+let spacing;
+let buttonRadius;
+let buttonLineWidth;
+let buttonSpacing;
+
 const buildPlayers = (t1, t2) => {
 	const totalTime = Math.max(t1, t2);
 	const players = [{
@@ -23,19 +36,26 @@ const buildPlayers = (t1, t2) => {
 	return players;
 };
 
-let mainWidth;
-let mainHeight;
-let x0, y0;
-let playerX0;
-let playerXOffset;
-let cx, cy;
-let clockRadius;
-let clockArcThickness;
+const gameIsOver = () => {
+	const loser = players.find(player => player.time === 0);
+	return loser != null;
+};
+
+const buildButton = ({
+	getPos,
+	draw,
+	onclick,
+}) => ({
+	getPos,
+	draw,
+	onclick,
+});
 
 const updatePositions = () => {
-	let spacingRatio = 0.05;
-	let mainRatio = 2 - spacingRatio;
-	let screenRatio = canvas.width/canvas.height;
+	const spacingRatio = 0.05;
+	const midSpaceRatio = 0.3;
+	const mainRatio = 2 - spacingRatio*2 + midSpaceRatio;
+	const screenRatio = canvas.width/canvas.height;
 	const screenIsWider = screenRatio > mainRatio;
 	if (screenIsWider) {
 		mainHeight = canvas.height;
@@ -44,15 +64,18 @@ const updatePositions = () => {
 		mainWidth = canvas.width;
 		mainHeight = mainWidth/mainRatio;
 	}
-	let spacing = spacingRatio*mainHeight;
+	spacing = spacingRatio*mainHeight;
 	clockRadius = mainHeight*0.5 - spacing;
 	x0 = (canvas.width - mainWidth)*0.5;
 	y0 = (canvas.height - mainHeight)*0.5;
 	cx = canvas.width*0.5;
 	cy = canvas.height*0.5;
 	playerX0 = x0 + spacing + clockRadius;
-	playerXOffset = clockRadius + spacing + clockRadius;
+	playerXOffset = (cx - playerX0)*2;
 	clockArcThickness = clockRadius*0.15;
+	buttonRadius = mainHeight*0.08;
+	buttonLineWidth = mainHeight*0.01;
+	buttonSpacing = buttonRadius*0.7;
 };
 
 const getSecondsNow = () => Date.now()/1000;
@@ -117,6 +140,7 @@ const drawTime = (time, x, y) => {
 const drawProgressArc = (progress, radius, thickness, x, y) => {
 	const ang0 = - Math.PI/2 + progress*Math.PI*2;
 	const ang1 = Math.PI*1.5;
+	ctx.lineCap = 'butt';
 	ctx.lineWidth = thickness;
 	ctx.beginPath();
 	ctx.arc(x, y, radius - thickness*0.5, ang0, ang1);
@@ -168,10 +192,22 @@ const drawPlayer = (index) => {
 	drawTime(player.time, x, y);
 };
 
+const drawButtons = () => {
+	for (let i=0; i<buttons.length; ++i) {
+		const [ x, y ] = buttonIndexToPos(i);
+		ctx.fillStyle = '#222';
+		ctx.beginPath();
+		ctx.arc(x, y, buttonRadius, 0, Math.PI*2);
+		ctx.fill();
+		buttons[i].draw(x, y);
+	}
+};
+
 const render = () => {
 	clear();
 	drawPlayer(0);
 	drawPlayer(1);
+	drawButtons();
 };
 
 const update = () => {
@@ -193,6 +229,13 @@ const frameLoop = () => {
 };
 
 const handleTouch = (x, y) => {
+	for (let i=0; i<buttons.length; ++i) {
+		const pos = buttonIndexToPos(i);
+		if (posIsInsideButton(x, y, pos)) {
+			buttons[i].onclick();
+			return;
+		}
+	}
 	const now = getSecondsNow();
 	let player = x >= cx ? players[1] : players[0];
 	if (player.time === 0 || player.other.time === 0) {
@@ -226,14 +269,23 @@ canvas.addEventListener('mousedown', e => {
 	handleTouch(x, y);
 });
 
+const timeUnits = {
+	h: 60*60,
+	hr: 60*60,
+	m: 60,
+	min: 60,
+	s: 1,
+	sec: 1,
+};
+
 const parseTimeParam = (value) => {
-	if (/m$/i.test(value)) {
-		return Number(value.replace(/m$/i, ''))*60;
-	}
-	if (/h$/i.test(value)) {
-		return Number(value.replace(/h$/i, ''))*60*60;
-	}
-	return Number(value);
+	const values = value.match(/\d+[a-z]*/ig);
+	const parsed = values.map(string => {
+		const [ _, number, unit ] = string.match(/(\d+)([a-z]*)/i);
+		if (!unit) return Number(number);
+		return Number(number)*timeUnits[unit];
+	});
+	return parsed.reduce((a, b) => a + b, 0);
 };
 
 const getInitialTimes = () => {
@@ -244,6 +296,121 @@ const getInitialTimes = () => {
 	return [ t1 || t, t2 || t ].map(val => parseTimeParam(val || '5m'));
 };
 
+const pauseButton = buildButton({
+	getPos: () => {
+		const x = cx;
+		const y = y0 + buttonRadius + spacing;
+		return [ x, y ];
+	},
+	draw: (x, y) => {
+		ctx.lineCap = 'round'
+		ctx.lineWidth = buttonLineWidth;
+		ctx.strokeStyle = '#fff';
+		const x0 = x - buttonRadius*0.25;
+		const y0 = y - buttonRadius*0.5;
+		const x1 = x + buttonRadius*0.25;
+		const y1 = y + buttonRadius*0.5;
+		ctx.beginPath();
+		ctx.moveTo(x0, y0);
+		ctx.lineTo(x0, y1);
+		ctx.moveTo(x1, y0);
+		ctx.lineTo(x1, y1);
+		ctx.stroke();
+	},
+	onclick: () => {
+		if (gameIsOver()) return;
+		const player = players.find(player => player.running);
+		if (!player) return;
+		endPlayerTurn(player, getSecondsNow());
+	},
+});
+
+const fullScreenButton = buildButton({
+	getPos: () => {
+		const x = cx;
+		const y = y0 + mainHeight - buttonRadius - spacing;
+		return [ x, y ];
+	},
+	draw: (x, y) => {
+		ctx.lineCap = 'round';
+		ctx.lineJoin = 'round';
+		ctx.lineWidth = buttonLineWidth;
+		ctx.strokeStyle = '#fff';
+		const len = buttonRadius*0.2;
+		const height = buttonRadius*0.75;
+		const x0 = x - buttonRadius*0.5;
+		const y0 = y - height*0.5;
+		const x1 = x0 + buttonRadius;
+		const y1 = y0 + height;
+		ctx.beginPath();
+		ctx.moveTo(x0, y0 + len);
+		ctx.lineTo(x0, y0);
+		ctx.lineTo(x0 + len, y0);
+		ctx.moveTo(x1 - len, y0);
+		ctx.lineTo(x1, y0);
+		ctx.lineTo(x1, y0 + len);
+		ctx.moveTo(x1, y1 - len);
+		ctx.lineTo(x1, y1);
+		ctx.lineTo(x1 - len, y1);
+		ctx.moveTo(x0, y1 - len);
+		ctx.lineTo(x0, y1);
+		ctx.lineTo(x0 + len, y1);
+		ctx.stroke();
+	},
+	onclick: () => {
+		document.documentElement.requestFullscreen();
+	},
+});
+
+const resetButton = buildButton({
+	draw: (x, y) => {
+		const rad = buttonRadius*0.5;
+		const ang0 = Math.PI*0.0;
+		const ang1 = Math.PI*1.5;
+		const tipX = x + Math.cos(ang1)*rad;
+		const tipY = y + Math.sin(ang1)*rad;
+		const arrSize = buttonRadius*0.2;
+		const drawArrow = (tilt) => {
+			const endX = tipX + Math.cos(ang1 + tilt)*arrSize;
+			const endY = tipY + Math.sin(ang1 + tilt)*arrSize;
+			ctx.moveTo(endX, endY);
+			ctx.lineTo(tipX, tipY);
+		};
+		ctx.strokeStyle = buttonLineWidth;
+		ctx.strokeStyle = '#fff';
+		ctx.beginPath();
+		ctx.arc(x, y, rad, ang0, ang1);
+		drawArrow(-Math.PI*0.35);
+		drawArrow(Math.PI*1.2);
+		ctx.stroke();
+	},
+	onclick: () => {
+		players.length = 0;
+		players.push(...buildPlayers(...getInitialTimes()));
+	},
+});
+
+const buttonIndexToPos = (index) => {
+	const n = buttons.length;
+	let x0 = cx;
+	let y0 = cy - (n*buttonRadius*2 + buttonSpacing*(n - 1))/2 + buttonRadius;
+	let stride = buttonRadius*2 + buttonSpacing;
+	return [ x0, y0 + stride*index ];
+};
+
+const posIsInsideButton = (x, y, [ buttonX, buttonY ]) => {
+	const dx = x - buttonX;
+	const dy = y - buttonY;
+	const distance = Math.sqrt(dx*dx + dy*dy);
+	return distance <= buttonRadius;
+};
+
 const players = buildPlayers(...getInitialTimes());
+const buttons = [
+	pauseButton,
+	fullScreenButton,
+	resetButton,
+];
+
 resizeCanvas();
 frameLoop();
